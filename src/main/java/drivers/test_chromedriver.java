270c1021d42df24db79714fe8b0a0a0f13b10b70
ceryigit <ceryigit@lely.com>
2024-08-22 17:39:00 +0300

    chromedriver added

---
+++ src/main/java/drivers/test_chromedriver.java
@@ -0,0 +1,20 @@
+package drivers;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class test_chromedriver {
+    public static void main(String[] args) {
+        // Chromedriver'ın yolunu belirtin
+        System.setProperty("webdriver.chrome.driver", "C:\\rd-automation-lesson\\src\\main\\java\\drivers\\chromedriver.exe");
+
+        // WebDriver nesnesini oluşturun
+        WebDriver driver = new ChromeDriver();
+
+        // WebDriver ile istediğiniz işlemleri gerçekleştirin
+        driver.get("https://www.google.com");
+
+        // Test sonunda tarayıcıyı kapatın
+        driver.quit();
+    }
+}
